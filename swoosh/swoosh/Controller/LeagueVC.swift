44c2149e8e49977d94438154e39d075dc96bf0f5
Herman Gábor <hermang@Hermans-MacBook.local>
2017-08-29 16:48:07 +0200

    segues

---
+++ swoosh/swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,35 @@
+//
+//  LeagueVC.swift
+//  swoosh
+//
+//  Created by Herman Gábor on 2017. 08. 29..
+//  Copyright © 2017. Herman Gábor. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
